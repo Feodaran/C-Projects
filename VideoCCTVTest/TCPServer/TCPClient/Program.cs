56a61cc156a914681c28b3ed6f197003f307a722
Steven Bierman <37386433+Feodaran@users.noreply.github.com>
2018-10-16 19:51:44 +0100

    Add files via upload

---
+++ VideoCCTVTest/TCPServer/TCPClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace TCPClient
+{
+    public static class TCPClient
+    {
+        public static void Main(string[] args)
+        {
+            System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+            clientSocket.Connect("VideoServerSim.exe", 13000);
+            while (true)
+            {
+                NetworkStream serverStream = clientSocket.GetStream();
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(Console.ReadLine());
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+            }
+
+
+            FileStream VideoServerSim = new FileStream("VideoServerSim.exe", FileMode.Open, FileAccess.Read, FileShare.Read);
+
+
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 13000);
+            listener.Start();
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                Task.Factory.StartNew(() =>
+                {
+                    //Create a new file for every connection
+                    using (var file = File.Create(Guid.NewGuid() + ".dat"))
+                    {
+                        client.GetStream().CopyTo(file);
+                    }
+
+                }, TaskCreationOptions.LongRunning);
+
+            }
+        }
+    }
+}
---
+++ VideoCCTVTest/TCPServer/TCPServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCPServer
+{
+    public static class TCPServer
+    {
+       
+        
+        public static void Main(string [] args)
+        {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ipAd, 13000);
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" ****** Server Started *****");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Accepted connection from client");
+
+
+            while ((true))
+            {
+                try
+                {
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesForm = new byte[10025];
+                    if (networkStream.DataAvailable)
+                    {
+                        //bytesForm is meant to be "bytesFrom".
+                        networkStream.Read(bytesForm, 0, (int)clientSocket.ReceiveBufferSize);
+                        string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesForm);
+                        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf('\0'));
+                        Console.WriteLine("Data from client - " + dataFromClient);
+                        networkStream.Flush();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+           
+        }
+    }
+}
